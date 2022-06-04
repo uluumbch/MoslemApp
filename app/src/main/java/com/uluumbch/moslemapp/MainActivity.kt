f638070c0a11ef946441d7ff742cc12c1e67ad88
uluumbch <uluum123@gmail.com>
2022-06-04 11:24:12 +0800

    menambahkan navigation drawer

--- app/src/main/java/com/uluumbch/moslemapp/MainActivity.kt
+++ app/src/main/java/com/uluumbch/moslemapp/MainActivity.kt
@@ -2,15 +2,60 @@ package com.uluumbch.moslemapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.close
+import android.system.Os.open
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.uluumbch.moslemapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-//    private lateinit var navController: NavController
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    //    private lateinit var navController: NavController
+    private lateinit var mToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // memunculkan tombol burger menu
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // untuk toggle open dan close navigation
+        val drawer_layout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        mToggle = ActionBarDrawerToggle(this, drawer_layout, R.string.open, R.string.close)
+        // tambahkan mToggle ke drawer_layout sebagai pengendali open dan close drawer
+        drawer_layout.addDrawerListener(mToggle)
+        mToggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        return mToggle.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.doa -> {
+                Toast.makeText(applicationContext, "Fragment doa", Toast.LENGTH_SHORT).show()
+            }
+            R.id.asma -> {
+                Toast.makeText(applicationContext, "fragment asmaulhusna", Toast.LENGTH_SHORT).show()
+            }
+            R.id.info -> {
+                Toast.makeText(applicationContext, "info", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+
 //        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 //        navController = navHostFragment.navController
 //        NavigationUI.setupActionBarWithNavController(this, navController)
