c68bbf9f1a773307607cc784b28ed189fd34d841
uluumbch <uluum123@gmail.com>
2022-06-07 18:43:35 +0800

    implementasi repository pattern

--- app/src/main/java/com/uluumbch/moslemapp/ui/MyViewModel.kt
+++ app/src/main/java/com/uluumbch/moslemapp/ui/MyViewModel.kt
@@ -10,12 +10,16 @@ import com.facebook.shimmer.ShimmerFrameLayout
 import com.uluumbch.moslemapp.network.AsmaulHusna
 import com.uluumbch.moslemapp.network.DoaHarian
 import com.uluumbch.moslemapp.network.MoslemAppApi
+import com.uluumbch.moslemapp.repository.MoslemAppRepository
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
 enum class ApiStatus { LOADING, ERROR, DONE }
 
 class MyViewModel : ViewModel() {
+    // Repository
+    private val MoslemAppRepository = MoslemAppRepository()
+
     // status
     private val _status = MutableLiveData<ApiStatus>()
     val status: LiveData<ApiStatus> = _status
@@ -41,7 +45,7 @@ class MyViewModel : ViewModel() {
         viewModelScope.launch {
             _status.value = ApiStatus.LOADING
             try {
-                _listdoaharian.value = MoslemAppApi.retrofitServiceApi.getDoa()
+                _listdoaharian.value = MoslemAppRepository.getDoa()
                 _status.value = ApiStatus.DONE
             } catch (e: Exception) {
                 Log.d("error", e.printStackTrace().toString())
@@ -54,7 +58,7 @@ class MyViewModel : ViewModel() {
     fun getAsmaulHusnaList(){
         viewModelScope.launch {
             try {
-                _listasmaulhusna.value = MoslemAppApi.retrofitServiceApi.getAsmaulHusna()
+                _listasmaulhusna.value = MoslemAppRepository.getAsmaulHusna()
             } catch (e: Exception) {
                 Log.d("error", e.printStackTrace().toString())
                 _listasmaulhusna.value = AsmaulHusna(listOf())
